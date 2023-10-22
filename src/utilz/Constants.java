1d05e1b4119659db4ee840421204b55c3d02a061
Duong Khanh Trang <trang.dk2823@gmail.com>
2023-10-22 15:49:20 +0700

    modified the constants class after changing the player sprites image

--- src/utilz/Constants.java
+++ src/utilz/Constants.java
@@ -10,33 +10,40 @@ public class Constants {
 	}
 	
 	public static class PlayerConstants{
-		public static final int ATTACK = 0;
-		public static final int DEAD = 1;
-		public static final int DEFEND = 2;
-		public static final int HURT = 3;
-		public static final int IDLE = 4;
-		public static final int RUNNING = 5;
-		public static final int JUMP = 6;
+		public static final int RUSH = 0;
+		public static final int ATTACK = 1;
+		public static final int GROUND = 2;
+		public static final int DEAD = 3;
+		public static final int FALLING = 4;
+		public static final int HIT = 5;
+		public static final int IDLE = 6;
+		public static final int JUMP = 7;
+		public static final int RUNNING = 8;
 		
+
 		public static int GetSpriteAmount(int player_action) {
-			
-			switch(player_action) {
-			
-			case ATTACK:
-				return 5;
-			case DEAD:
-				return 6;
-			case DEFEND:
-				return 5;
-			case HURT:
-				return 2;
-			case IDLE:
-				return 4;
-			case RUNNING:
-				return 6;
-			case JUMP:
-			default:
-				return 7;
+			switch (player_action) {
+				
+				case RUNNING:
+					return 10;
+				case IDLE:
+					return 10;
+				case HIT:
+					return 1;
+				case JUMP:
+					return 3;
+				case GROUND:
+					return 1;
+				case FALLING:
+					return 3;
+				case ATTACK:
+					return 10;
+				case DEAD:
+					return 10;
+				case RUSH:
+					return 2;
+				default:
+					return 1;
 			}
 		}
 	}
