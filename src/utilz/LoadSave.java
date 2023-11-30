924e4b63de4fc627dbee12ec2be7a323b84c20af
Nguyen Quang Sang <ITDSIU21113@student.hcmiu.edu.vn>
2023-11-30 12:05:44 +0700

    add the backgroundImg attribute to utilz

--- src/utilz/LoadSave.java
+++ src/utilz/LoadSave.java
@@ -21,6 +21,7 @@ public class LoadSave {
 	public static final String SOUND_BUTTONS = "sound_button.png";
 	public static final String URM_BUTTONS = "urm_buttons.png";
 	public static final String VOLUME_BUTTONS = "volume_buttons.png";
+	public static final String PLAYING_BG_IMG_MAP1 = "playing_bg_image_map1.png";
 
 	public static BufferedImage GetSpriteAtlas(String fileName) {
 		BufferedImage img = null;
