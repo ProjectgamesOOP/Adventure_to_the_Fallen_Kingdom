972fa428d9d021828256d3bcf3c193ada5031107
Nguyen Quang Sang <ITDSIU21113@student.hcmiu.edu.vn>
2023-12-25 20:23:38 +0700

    on the drawHitBox of player

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -148,8 +148,8 @@ public class Player extends Entity {
 		g.drawImage(animations[state][aniIndex],
 				(int)(hitbox.x - xDrawOffset) - 30 - lvlOffset + flipX ,
 				(int)(hitbox.y - yDrawOffset) - 70,width * flipW, height, null);
-//		drawHitbox(g, lvlOffset);
-//		drawAttackBox(g, lvlOffset);
+		drawHitbox(g, lvlOffset);
+		drawAttackBox(g, lvlOffset);
 		drawUI(g);
 	}
 
